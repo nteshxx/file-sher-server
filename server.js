488b60c2ccb17b8c900940ed4e157f53494ac079
Nitesh Yadav <60145175+nteshxx@users.noreply.github.com>
2022-03-22 11:26:03 +0530

    changed the upload response link

--- server.js
+++ server.js
@@ -4,21 +4,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 const cors = require('cors');
-// Cors 
+// Cors
 const corsOptions = {
-  origin: process.env.ALLOWED_CLIENTS.split(',')
+  origin: process.env.ALLOWED_CLIENTS.split(','),
   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-}
+};
 
-// Default configuration looks like
-// {
-//     "origin": "*",
-//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     "preflightContinue": false,
-//     "optionsSuccessStatus": 204
-//   }
-
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 app.use(express.static('public'));
 
 const connectDB = require('./config/db');
@@ -29,10 +21,9 @@ app.use(express.json());
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
-// Routes 
+// Routes
 app.use('/api/files', require('./routes/files'));
 // app.use('/files', require('./routes/show'));
 app.use('/files/download', require('./routes/download'));
-
 
 app.listen(PORT, console.log(`Listening on port ${PORT}.`));
